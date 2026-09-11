d999319109b5de92437185289d05b218056394bc
Brendon Thiede <brendon.thiede@gmail.com>
2026-09-11 09:56:10 -0400

    feat: adding the ability to collapse sections

--- scc/sources/__init__.py
+++ scc/sources/__init__.py
@@ -2,5 +2,5 @@
 Order here is the dashboard order."""
 from scc.sources import gcal, github_prs, incidentio, notion, sentry, slack_support
 
-SOURCES = (gcal, sentry, slack_support, incidentio, github_prs, notion)
+SOURCES = (sentry, slack_support, incidentio, github_prs, notion, gcal)
 BY_NAME = {s.NAME: s for s in SOURCES}
